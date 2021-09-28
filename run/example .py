195aac7a6b2d1e44ec89fae2c70ea6e92379464f
MaeganBoosten <70945250+MaeganBoosten@users.noreply.github.com>
2021-09-28 16:27:14 +0200

    Update example .py

--- run/example .py
+++ run/example .py
@@ -2,10 +2,11 @@ import sys
 import os
 import numpy as np
 import pickle
+import logging
+import re
 
 sys.path.insert(0, 'evoman')
 sys.path.insert(0, 'agents')
-
 
 from environment import Environment
 from controller import Controller
@@ -33,3 +34,50 @@ if __name__=='__main__':
     env.update_parameter('contacthurt', 'player')
 
     f, p, e, t = env.play()
+
+##Collecting  Logs
+
+regex = re.compile(r'(\d+)')
+
+def open_file(filepath, generation):
+    with open(os.path.join(filepath, generation), 'rb') as fp:
+        population = pickle.load(fp)
+    return population
+
+if __name__=='__main__':
+    # Defining controller
+    trained = [1, 5, 7, 8]
+    tested  = [3, 4, 2, 6]
+
+    experiment_name = 'test'
+
+    filepath = os.path.join('models', experiment_name)
+    files = os.listdir(filepath)
+
+    generations = {int(regex.findall(file)[0]): file for file in files}
+    generations =  sorted(generations.items(), key=lambda x: x[0])
+    
+    results = dict()
+
+    for generation in generations:
+        population = open_file(filepath, generation[1])
+        networks = list(population.keys())
+
+        for network in networks:
+            
+            env = Environment(speed="fastest",
+                            enemymode="static",
+                            player_controller=network,
+                            enemies=tested,
+                            multiplemode="yes",
+                            level=2,
+                            logs='off')
+
+            env.update_parameter('contacthurt', 'player')
+
+            f, p, e, t = env.play()
+            results[(generation[0], network)] = [f, p, e, t]
+    
+    with open(os.path.join(filepath, 'all_results_tested.pkl'), 'wb') as fp:
+            pickle.dump(results, fp)
+	
