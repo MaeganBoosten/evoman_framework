cfa07508167cf5b4974b5ec7482982610a406da7
MaeganBoosten <70945250+MaeganBoosten@users.noreply.github.com>
2021-09-28 16:23:29 +0200

    Create example .py

---
+++ run/example .py
@@ -0,0 +1,35 @@
+import sys
+import os
+import numpy as np
+import pickle
+
+sys.path.insert(0, 'evoman')
+sys.path.insert(0, 'agents')
+
+
+from environment import Environment
+from controller import Controller
+
+experiment_name = 'test'
+
+if not os.path.exists(experiment_name):
+	os.makedirs(experiment_name)
+
+if __name__=='__main__':
+    # Defining controller
+    with open('models/experiment_pop_100_gen_100', 'rb') as fp:
+        population = pickle.load(fp)
+
+    population_ordered = dict(sorted(population.items(), key=lambda x: x[1], reverse=True))
+
+    env = Environment(speed="normal",
+                      enemymode="static",
+                      player_controller=list(population_ordered.keys())[0],
+                      enemies=[1, 2, 3, 4, 5, 6, 7, 8],
+                      multiplemode="yes",
+                      level=2,
+                      logs='on')
+
+    env.update_parameter('contacthurt', 'player')
+
+    f, p, e, t = env.play()
